6af871b25bb07078c5e2e298ddb60a63425f2597
Artem <krutoypan3@gmail.com>
2020-06-14 20:01:35 +0300

    ReDesign code

--- Vk_bot.py
+++ Vk_bot.py
@@ -3,47 +3,39 @@ import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.utils import get_random_id
 import random
-import nhentaidl
-
+# Импорт API ключа из отдельного файла
 f = open('D://VK_BOT/APIKEY.txt', 'r')
 APIKEYSS = f.read()
 
 
 def main():
-    """ Пример использования bots longpoll
-        https://vk.com/dev/bots_longpoll
-    """
-
+    # Авторизация под именем сообщества
     vk_session = vk_api.VkApi(
         token=APIKEYSS)
-
+    # Указываем id сообщества
     longpoll = VkBotLongPoll(vk_session, '196288744')
     vk = vk_session.get_api()
 
-    
+    # Постоянный листинг сообщений
     for event in longpoll.listen():
         # Проверка на приход сообщения
         if event.type == VkBotEventType.MESSAGE_NEW:
-
             # Логика ответов
             if event.obj.text == "Братик привет" or event.obj.text == "братик привет":
                 vk.messages.send(
                     peer_id=event.object.peer_id,
-
                     random_id=get_random_id(),
                     message=("Приветик")
                 )
             elif event.obj.text == "Братик пока" or event.obj.text == "братик пока":
                 vk.messages.send(
                     peer_id=event.object.peer_id,
-
                     random_id=get_random_id(),
                     message=("Прощай")
                 )
             elif event.obj.text == "-инфо" or event.obj.text == "-инфо":
                 vk.messages.send(
                     peer_id=event.object.peer_id,
-
                     random_id=get_random_id(),
                     message=("Мой разработчик - студет АГПУ Оганесян Артем.\nВсе вопросы по реализации к нему: "
                              "vk.com/aom13")
@@ -51,10 +43,9 @@ def main():
             elif event.obj.text == "-команды" or event.obj.text == "-команды" or event.obj.text == "братик" or event.obj.text == "Братик":
                 vk.messages.send(
                     peer_id=event.object.peer_id,
-
                     random_id=get_random_id(),
                     message=("Команды: просто напишите няша и нужную вам команду\n-лоли "
-                             "\n-команды\n-инфо\n-хентай\n-арты\n-стикер\n-игра")
+                             "\n-команды\n-инфо\n-хентай\n-арты\n-стикер\n-игры")
                 )
             elif event.obj.text == "-лоли" or event.obj.text == "-лоли":
                 upload = vk_api.VkUpload(vk)
@@ -64,8 +55,11 @@ def main():
                 photo_id = photo[0]['id']
                 access_key = photo[0]['access_key']
                 attachment = f'photo{owner_id}_{photo_id}_{access_key}'
-                vk.messages.send(peer_id=event.object.peer_id, random_id=0, attachment=attachment)
-
+                vk.messages.send(
+                    peer_id=event.object.peer_id,
+                    random_id=0,
+                    attachment=attachment
+                )
             elif event.obj.text == "-арты" or event.obj.text == "-арты":
                 upload = vk_api.VkUpload(vk)
                 photo = upload.photo_messages(
@@ -74,9 +68,12 @@ def main():
                 photo_id = photo[0]['id']
                 access_key = photo[0]['access_key']
                 attachment = f'photo{owner_id}_{photo_id}_{access_key}'
-                vk.messages.send(peer_id=event.object.peer_id, random_id=0, attachment=attachment)
-
-            elif event.obj.text == "-Хентай" or event.obj.text == "-хентай":
+                vk.messages.send(
+                    peer_id=event.object.peer_id,
+                    random_id=0,
+                    attachment=attachment
+                )
+            elif event.obj.text == "-хент" or event.obj.text == "-хентай":
                 upload = vk_api.VkUpload(vk)
                 photo = upload.photo_messages(
                     'D://VK_BOT/Hentai/ (' + str(random.randint(1, 419)) + ').jpg')  # Отправляет с пк файлы в беседы
@@ -84,8 +81,11 @@ def main():
                 photo_id = photo[0]['id']
                 access_key = photo[0]['access_key']
                 attachment = f'photo{owner_id}_{photo_id}_{access_key}'
-                vk.messages.send(peer_id=event.object.peer_id, random_id=0, attachment=attachment)
-
+                vk.messages.send(
+                    peer_id=event.object.peer_id,
+                    random_id=0,
+                    attachment=attachment
+                )
             elif event.obj.text == "-стикер" or event.obj.text == "-стик":
                 upload = vk_api.VkUpload(vk)
                 photo = upload.photo_messages(
@@ -94,24 +94,24 @@ def main():
                 photo_id = photo[0]['id']
                 access_key = photo[0]['access_key']
                 attachment = f'photo{owner_id}_{photo_id}_{access_key}'
-                vk.messages.send(peer_id=event.object.peer_id, random_id=0, attachment=attachment)
-
-            elif event.obj.text == "-Игра" or event.obj.text == "-игра":
                 vk.messages.send(
                     peer_id=event.object.peer_id,
-
+                    random_id=0,
+                    attachment=attachment
+                )
+            elif event.obj.text == "-список игр" or event.obj.text == "-игры":
+                vk.messages.send(
+                    peer_id=event.object.peer_id,
                     random_id=get_random_id(),
                     message=(
                         "Выберите игру ниже из списка: \n-угадай число\n")
                 )
-
-            elif event.obj.text == "-Угадай число" or event.obj.text == "-угадай число":
+            elif event.obj.text == "-угадай число":
                 vk.messages.send(
                     peer_id=event.object.peer_id,
                     random_id=get_random_id(),
                     message=("Какое число я загадал? От 1 до 5\nНаграда: экслюзивный хентай"))
                 chislos = random.randint(1, 5)
-
                 for event in longpoll.listen():
                     # Проверка на приход сообщения
                     if event.type == VkBotEventType.MESSAGE_NEW:
@@ -120,7 +120,7 @@ def main():
                                 vk.messages.send(
                                     peer_id=event.object.peer_id,
                                     random_id=get_random_id(),
-                                    message=("Правильно"))
+                                    message=("Правильно! Вот твоя награда:"))
                                 upload = vk_api.VkUpload(vk)
                                 photo = upload.photo_messages('D://VK_BOT/Hentai/ (' + str(
                                     random.randint(1, 101)) + ').jpg')  # Отправляет с пк файлы в беседы
@@ -135,50 +135,34 @@ def main():
                                     random_id=get_random_id(),
                                     message=("Неправильно\n правильный ответ:" + str(chislos)))
                     break
-
+        # Вывод в консоли информации о происходящем
         if event.type == VkBotEventType.MESSAGE_NEW:
             print('Новое сообщение:')
-
             print('Для меня от: ', end='')
-
             print(event.obj.from_id)
-
             print('Текст:', event.obj.text)
             print()
             print('ok')
-
         elif event.type == VkBotEventType.MESSAGE_REPLY:
             print('Новое сообщение:')
-
             print('От меня для: ', end='')
-
             print(event.obj.peer_id)
-
             print('Текст:', event.obj.text)
             print()
-
         elif event.type == VkBotEventType.MESSAGE_TYPING_STATE:
             print('Печатает ', end='')
-
             print(event.obj.from_id, end=' ')
-
             print('для ', end='')
-
             print(event.obj.to_id)
             print()
-
         elif event.type == VkBotEventType.GROUP_JOIN:
             print(event.obj.user_id, end=' ')
-
             print('Вступил в группу!')
             print()
-
         elif event.type == VkBotEventType.GROUP_LEAVE:
             print(event.obj.user_id, end=' ')
-
             print('Покинул группу!')
             print()
-
         else:
             print(event.type)
             print()
