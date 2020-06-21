194b1b5fb03fc95f07924f26be386eb30049d665
Artem <krutoypan3@gmail.com>
2020-06-22 02:56:19 +0300

    Delete console message print

--- Vk_bot.py
+++ Vk_bot.py
@@ -129,37 +129,6 @@ def main():
                     random_id=0,
                     attachment=attachment
                 )
-        # Вывод в консоли информации о происходящем
-        if event.type == VkBotEventType.MESSAGE_NEW:
-            print('Новое сообщение:')
-            print('Для меня от: ', end='')
-            print(event.obj.from_id)
-            print('Текст:', event.obj.text)
-            print()
-            print('ok')
-        elif event.type == VkBotEventType.MESSAGE_REPLY:
-            print('Новое сообщение:')
-            print('От меня для: ', end='')
-            print(event.obj.peer_id)
-            print('Текст:', event.obj.text)
-            print()
-        elif event.type == VkBotEventType.MESSAGE_TYPING_STATE:
-            print('Печатает ', end='')
-            print(event.obj.from_id, end=' ')
-            print('для ', end='')
-            print(event.obj.to_id)
-            print()
-        elif event.type == VkBotEventType.GROUP_JOIN:
-            print(event.obj.user_id, end=' ')
-            print('Вступил в группу!')
-            print()
-        elif event.type == VkBotEventType.GROUP_LEAVE:
-            print(event.obj.user_id, end=' ')
-            print('Покинул группу!')
-            print()
-        else:
-            print(event.type)
-            print()
 
 
 if __name__ == '__main__':
