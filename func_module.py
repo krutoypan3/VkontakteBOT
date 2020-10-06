8db1063eadf143513249fb960e8dcc5caabf7382
Artem <krutoypan3@gmail.com>
2020-10-06 19:46:01 +0300

    fix-test

--- func_module.py
+++ func_module.py
@@ -8,12 +8,12 @@ import time
 import requests
 import urllib3
 import vk_api
+from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 from googletrans import Translator
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.utils import get_random_id
-import AnimeGoParser
 import Dict
 import db_module
 
@@ -75,10 +75,89 @@ def zapros_ft_vd():
     photo_gitl = vk_SERVISE.photos.get(owner_id='-' + '196288744', album_id=273184565, count=1000)
 
 
+class AnimeGo:
+    print('Создан экземпляр класса AnimeGo')
+    def __init__(self, Anime_type):
+        if Anime_type == 'ongoing':
+            self.url = 'https://animego.org/anime/filter/status-is-ongoing-or-released/apply?&page='
+            self.col = 3
+            print('Инициализация класса AnimeGo ongoing')
+        elif Anime_type == 'finish':
+            self.url = 'https://animego.org/anime/filter/status-is-released/apply?&direction=desc&page='
+            self.col = 100
+            print('Инициализация класса AnimeGo finish')
+
+    def random_anime(self):
+        Anime = []
+        url = self.url
+        print('Инициализация функции AnimeGo.random_anime')
+        for i in range(self.col):
+            ani = url + str(i+1)
+            print(ani)
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.90 Safari/537.36,', 'Origin': 'http://example.com', 'Referer': 'http://example.com/some_page'}
+            response = requests.request("get", ani, headers=headers)
+            print(response)
+            print(response.status_code)
+            try:
+                soup = BeautifulSoup(response.text, "lxml")
+                anima = soup.find_all('div', {'id': 'anime-list-container'})  # Получаем все таблицы с вопросами
+                for animeshka in anima:
+                    for g in range(len(animeshka.contents)):
+                        try:
+                            Anime_janr = []
+                            Anime_name = animeshka.contents[g].next.contents[1].contents[0].next.next  # Название
+                            Anime_type = animeshka.contents[g].next.contents[1].contents[2].next.next.next  # Тип аниме
+                            Anime_year = animeshka.contents[g].next.contents[1].contents[2].contents[2].next.next  # год
+                            Anime_urls = animeshka.contents[g].next.contents[1].contents[0].next.attrs['href']  # Ссылка
+
+                            try:
+                                Anime_rait = animeshka.contents[g].next.contents[0].contents[2].contents[0].contents[1].next  # рейтинг
+                            except IndexError:
+                                Anime_rait = 'без рейтинга'
+
+
+                            '''def series():
+                                response2 = requests.request("GET", Anime_urls)
+                                soup2 = BeautifulSoup(response2.text, "lxml")
+                                anima2 = soup2.find_all('div', {'class': 'anime-info'})  # Получаем все таблицы с вопросами
+                                Anime_ser = anima2[0].contents[0].contents[3].next
+                                return Anime_ser
+                            Anime_seri = series()'''
+
+                            try:
+                                for k in range(len(animeshka.contents[g].next.contents[1].contents[2].contents[4].contents)):
+                                    if k % 2 == 0:
+                                        Anime_janr.append(animeshka.contents[g].next.contents[1].contents[2].contents[4].contents[k].next)
+                            except IndexError:
+                                Anime_janr.append('Информация о жанрах отсутствует')
+
+                            try:
+                                Anime_pict = animeshka.contents[g].next.contents[0].contents[0].next.next['data-original']  # Картинка
+                            except KeyError:
+                                Anime_pict = 'https://upload.wikimedia.org/wikipedia/ru/0/04/%D0%9D%D0%95%D0%A2_%D0%94%D0%9E%D0%A1%D0%A2%D0%A3%D0%9F%D0%9D%D0%9E%D0%93%D0%9E_%D0%98%D0%97%D0%9E%D0%91%D0%A0%D0%90%D0%96%D0%95%D0%9D%D0%98%D0%AF.jpg'
+
+                            if len(animeshka.contents[g].next.contents[1].contents[3].contents) > 0:
+                                Anime_dict = animeshka.contents[g].next.contents[1].contents[3].contents[0]
+                            else:
+                                Anime_dict = 'Описание отсутствует'
+                            while '\n' in Anime_dict or '  ' in Anime_dict:
+                                Anime_dict = Anime_dict.replace("\n", "")
+                                Anime_dict = Anime_dict.replace("  ", " ")
+
+                            Anime.append([Anime_name, Anime_pict, Anime_urls, Anime_dict, Anime_type, Anime_year, Anime_janr, Anime_rait])
+                        except IndexError as ERROR:
+                            print(ERROR)
+            except UnicodeEncodeError as ERROR:
+                print(ERROR)
+        print(len(Anime))
+        return Anime
+
+
 print('Импортируем список онгоингов...')
-AnimeOngoing = AnimeGoParser.AnimeGo('ongoing').random_anime()
+AnimeOngoing = AnimeGo('ongoing').random_anime()
 print('Импортируем список всех аниме...')
-AnimeFinish = AnimeGoParser.AnimeGo('finish').random_anime()
+AnimeFinish = AnimeGo('finish').random_anime()
 print('Импортируем фото из альбомов...')
 zapros_ft_vd()
 
