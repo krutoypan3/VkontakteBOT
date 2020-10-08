e84f648cf014a2915513a94e335dd8093b4931fd
Artem <krutoypan3@gmail.com>
2020-10-09 02:21:25 +0300

    test 3

--- func_module.py
+++ func_module.py
@@ -169,7 +169,7 @@ try:
             'Host': 'yummyanime.club',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0)   Gecko/20100101 Firefox/69.0',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-            'Accept-Language': 'ru,en-US;q=0.5',
+            'Accept-Language': 'ru,ru-RU;q=0.5',
             'Accept-Encoding': 'gzip, deflate, br',
             'DNT': '1',
             'Connection': 'keep-alive',
