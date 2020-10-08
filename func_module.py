a06182e6375dad578ea3d6cd4446f9a04078ea05
Artem <krutoypan3@gmail.com>
2020-10-09 02:37:48 +0300

    test 7

--- func_module.py
+++ func_module.py
@@ -166,7 +166,11 @@ try:
     def get_session(host):
         session = requests.Session()
         session.headers = {
-            'Host': host,
+            ':authoriti': host,
+            ':method': 'GET',
+            ':path': '/',
+            ':scheme': 'https',
+            
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
             'Accept-Language': 'ru,ru-ru;q=0.5',
